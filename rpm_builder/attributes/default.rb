c6cb674f34cce8086b9993bba43a83019a36ad13
Christian Requena <requena@amazon.de>
2013-03-20 12:38:41 +0100

    fix package url

--- rpm_builder/attributes/default.rb
+++ rpm_builder/attributes/default.rb
@@ -11,7 +11,7 @@ default[:ruby_package_builder][:rpm][:prefix] = '/usr/local'
 default[:ruby_package_builder][:rpm][:configure] = '--enable-shared --disable-install-doc'
 default[:ruby_package_builder][:rpm][:arch] = node[:kernel][:machine] == 'x86_64' ? 'amd64' : 'i386'
 default[:ruby_package_builder][:rpm][:base_url] = "http://ftp.ruby-lang.org/pub/ruby/"
-default[:ruby_package_builder][:rpm][:package_url] = "#{node[:ruby_package_builder][:rpm][:base_url]}/#{node[:ruby_package_builder][:rpm][:basename].match(/.\../)[0]}/#{node[:ruby_package_builder][:rpm][:basename]}.tar.bz2"
+default[:ruby_package_builder][:rpm][:package_url] = "#{node[:ruby_package_builder][:rpm][:base_url]}/#{node[:ruby_package_builder][:ruby][:version].match(/.\../)[0]}/#{node[:ruby_package_builder][:rpm][:basename]}.tar.bz2"
 default[:ruby_package_builder][:rpm][:package_name] = "ruby1.9_#{node[:ruby_package_builder][:rpm][:version]}-#{node[:ruby_package_builder][:rpm][:patch]}.#{node[:ruby_package_builder][:rpm][:pkgrelease]}_#{node[:ruby_package_builder][:rpm][:arch]}.rpm"
 default[:ruby_package_builder][:rpm][:cleanup] = false
 default[:ruby_package_builder][:rpm][:s3] = {}
